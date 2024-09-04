e77811b70d959d6003850c1126c45f2e5156fba4
Herik <herik@wyco.com>
2024-09-04 15:38:15 +0700

    Lab8

---
+++ Lab8_1_reference.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void tripleCallByValue(int );
+void tripleCallByReference(int &);
+
+int main(){
+    int x;
+    cout<<"Enter an integer: ";
+    cin>>x;
+
+    cout<<"Value before call to tripleCallByValue() is : "<< x<<endl;
+    tripleCallByValue(x);
+    cout<< "Value (in main) after tripleCallByValue() is : "<< x<< endl<<endl;
+
+
+
+    cout<<"Value before call to tripleCallByReference() is : "<< x<<endl;
+    tripleCallByReference(x);
+    cout<< "Value (in main) after tripleCallByReference() is : "<< x<< endl;
+}
+
+void tripleCallByValue(int a){
+    int y=a*3;
+    cout<< "Value returned from tripleCallByValue() is : "<<y<<endl;
+}
+
+void tripleCallByReference(int &a){
+    int &y=a;
+    y=a*3;
+}
---
+++ Lab8_2_CylinderVol.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+float calVol(float , float );
+
+int main(){
+    float r=0,h=0;
+    char more;
+    do{
+        cout<<"Please enter radius and height of a cylinder: ";
+        cin>>r>>h;
+        
+        while(r<0 || h< 0){
+            cout<<"Invalid Input. Try Again."<<endl;
+            cout<<"Please enter radius and height of a cylinder: ";
+            cin>>r>>h;
+        }
+        if(r==0){
+            r=2;
+        }
+        if(h==0){
+            h=2;
+        }
+
+        cout<<"The volume of the cylinder is "<< fixed << setprecision(3)<<calVol(r,h)<<endl;
+        cout<<"Would u like to continue?(Press Y) ";
+        cin>>more;
+    }while(more == 'Y');
+    cout<<"Good Bye!";
+    return 0;
+}
+
+
+
+float calVol(float a=2, float b=2){
+    float volume = M_PI * pow(a,2) * b;
+    return volume;
+}
